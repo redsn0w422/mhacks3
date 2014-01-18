c8c678f09b7e30763fdc259157e37fab8c2ee3da
Yasha Mostofi <yasha.mostofi@gmail.com>
2014-01-18 01:15:02 -0500

    SO MAD

--- phpscript.php
+++ phpscript.php
@@ -1,23 +1,21 @@
 <?php
-echo "HI1";
+
 echo <<<_END
 
 <html>
 	<head>
-		<title>Pic Print - Step 2</title>
-		<meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width" />        
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+		<title>Pic Print - Step 2</title> 
+		
+		<link rel='stylesheet' type='text/css' href='stylesheet.css'/>
 
-        <link rel='stylesheet' type='text/css' href='stylesheet.css'/>
 	</head>
+	
 	</body>
-	<div id="container>
+	<div id="container">
 		<h3>Pic Print Part 2</h3>
 
 _END;
-echo "HI2";
+
 if (isset($_POST['username']))
     $username = $_POST['username'];
 else
@@ -33,62 +31,52 @@ if (isset($_POST['SOCIAL_NETWORK']))
 else
     $SOCIAL_NETWORK = "";
 
-echo "HI3";
-    
+
+
 // condition to check if user can move on or not,
 // return to login page if user cannot.
-if (($username == "") or ($password == "") or ($SOCIAL_NETWORK == ""))
-{
-	$CONTINUE = false;
-	// error occurred, something obviously didn't work correctly.
-}	
-else
-{
-	$CONTINUE = true;
+if (($username == "") or ($password == "") or ($SOCIAL_NETWORK == "")) {
+    $CONTINUE = false;
+    // error occurred, something obviously didn't work correctly.
+} else {
+    $CONTINUE = true;
 }
-echo "HI4";
-if (!($CONTINUE))
-{
-	// show the user the form again, allow them to attempt filling in 
-	// ALL the information
-	echo "Information is incomplete, please try again.";
-	
-echo <<<_END
+
+if (!($CONTINUE)) {
+    // show the user the form again, allow them to attempt filling in 
+    // ALL the information
+    echo "Information is incomplete, please try again.";
+	echo <<<_END
 
     	<form action="phpscript.php" method="POST" name="introForm" id="introForm">
-    		<p>Username:</p>
+    		Username:
     		<input type="text" id="username" name="username" placeholder="username..."/>
     		</br>
-    		<p>Password:</p>
+    		Password:
     		<input type="password" id="password" name="password" />
     		</br>
+    		</br>
     		<input type="radio" name="SOCIAL_NETWORK" value="fb" />
-    		<p>Facebook</p>
+    		Facebook
     		<input type="radio" name="SOCIAL_NETWORK" value="insta" />
-    		<p>Instragram</p>
+    		Instragram
+    		</br>
     		<input type="submit" value="Submit!" />
     	</form>
 _END;
-	
+
+} else {
+    echo $username . $password . $SOCIAL_NETWORK;
+    // decide which social network API to use
+    if ($SOCIAL_NETWORK == "fb") {
+        // use facebook
+    } else if ($SOCIAL_NETWORK == "insta") {
+        // use instagram
+    } else {
+        // what is this I don't even
+    }
 }
-else
-{
-	echo $username . $password . $SOCIAL_NETWORK;
-	// decide which social network API to use
-	if ($SOCIAL_NETWORK == "fb")
-	{
-		// use facebook
-	}
-	else if ($SOCIAL_NETWORK == "insta")
-	{
-		// use instagram
-	}
-	else
-	{
-		// what is this I don't even
-	}
-}
-echo "HI5";
+
 echo <<<_END
 	</body>
 </html>
