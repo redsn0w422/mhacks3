943fe9dbf1b01a0c53458e47fade6fd411ba5434
Yasha Mostofi <yasha.mostofi@gmail.com>
2014-01-18 14:10:59 -0500

    Revert 8d1ee05..0bad076
    This rolls back to commit 8d1ee05369955284cd5e3e13f23c476520f07fc2.

--- logout.php
+++ logout.php
@@ -4,6 +4,6 @@ $facebook = new Facebook(array(
   'appId'  => '1414991652077072',
   'secret' => 'd7f434a8b143b848819e782d7d184645',
 ));
-$facebook->destroySession();
-header( "location:index.php" );
+session_destroy();
+header( "location:picprint.php" );
 ?>
