4da089eb351581cd81373b2b31ba9d9304cf1f77
briantruong777 <briantruong777@gmail.com>
2014-01-17 22:37:35 -0600

    switched to new instagram php library

--- insta/callback.php
+++ insta/callback.php
@@ -1,55 +1,126 @@
 <?php
 
-session_start();
-require_once 'Instagram.php';
+/**
+ * Instagram PHP API
+ * 
+ * @link https://github.com/cosenary/Instagram-PHP-API
+ * @author Christian Metz
+ * @since 01.10.2013
+ */
 
-$config = array(
-	'client_id' => '359a0cb55e014c2a853b77fed4769564',
-	'client_secret' => 'a03c4453898846abbdadca739b4c1dde',
-	'grant_type' => 'authorization_code',
-	'redirect_uri' => 'http://yashamostofi.com/drinkspls/insta/callback.php'
-);
+require_once 'instagram.class.php';
 
-// Instantiate the API handler object
-$instagram = new Instagram($config);
-$accessToken = $instagram->getAccessToken();
-$_SESSION['InstagramAccessToken'] = $accessToken;
+// initialize class
+$instagram = new Instagram(array(
+  'apiKey'      => '359a0cb55e014c2a853b77fed4769564',
+  'apiSecret'   => 'a03c4453898846abbdadca739b4c1dde',
+  'apiCallback' => 'http://yashamostofi.com/drinkspls/insta/callback.php'
+));
 
-$instagram->setAccessToken($_SESSION['InstagramAccessToken']);
-$popular = $instagram->getPopularMedia();
+// receive OAuth code parameter
+$code = $_GET['code'];
 
-// After getting the response, let's iterate the payload
-$response = json_decode($popular, true);
+// check whether the user has granted access
+if (isset($code)) {
+
+  // receive OAuth token object
+  $data = $instagram->getOAuthToken($code);
+  $username = $username = $data->user->username;
+  
+  // store user access token
+  $instagram->setAccessToken($data);
+
+  // now you have access to all authenticated user methods
+  $result = $instagram->getUserMedia();
+
+} else {
+
+  // check whether an error occurred
+  if (isset($_GET['error'])) {
+    echo 'An error occurred: ' . $_GET['error_description'];
+  }
+
+}
+
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<link rel="stylesheet" type="text/css" href="styles.css" />
-<title>Instagram API PHP Implementation demo // Popular Media</title>
-</head>
 
-<body>
-
-<h1>Popular Media</h1>
-
-<?
-    foreach ($response['data'] as $data) {
-        $link = $data['link'];
-        $id = $data['id'];
-        $caption = $data['caption']['text'];
-        $author = $data['caption']['from']['username'];
-        $thumbnail = $data['images']['thumbnail']['url'];
-    ?>
-    <div class="photo">
-        <a href="pic.php?id=<?= $id ?>"><span></span><img src="<?= $thumbnail ?>" title="<?= htmlentities($caption) ?>" alt="<?= htmlentities($caption) ?>" /></a>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Instagram - photo stream</title>
+    <link href="https://vjs.zencdn.net/4.2/video-js.css" rel="stylesheet">
+    <link href="assets/style.css" rel="stylesheet">
+    <script src="https://vjs.zencdn.net/4.2/video.js"></script>
+  </head>
+  <body>
+    <div class="container">
+      <header class="clearfix">
+        <img src="assets/instagram.png" alt="Instagram logo">
+        <h1>Instagram photos <span>taken by <? echo $data->user->username ?></span></h1>
+      </header>
+      <div class="main">
+        <ul class="grid">
+        <?php
+          // display all user likes
+          foreach ($result->data as $media) {
+            $content = "<li>";
+            
+            // output media
+            if ($media->type === 'video') {
+              // video
+              $poster = $media->images->low_resolution->url;
+              $source = $media->videos->standard_resolution->url;
+              $content .= "<video class=\"media video-js vjs-default-skin\" width=\"250\" height=\"250\" poster=\"{$poster}\"
+                           data-setup='{\"controls\":true, \"preload\": \"auto\"}'>
+                             <source src=\"{$source}\" type=\"video/mp4\" />
+                           </video>";
+            } else {
+              // image
+              $image = $media->images->low_resolution->url;
+              $content .= "<img class=\"media\" src=\"{$image}\"/>";
+            }
+            
+            // create meta section
+            $avatar = $media->user->profile_picture;
+            $username = $media->user->username;
+            $comment = $media->caption->text;
+            $content .= "<div class=\"content\">
+                           <div class=\"avatar\" style=\"background-image: url({$avatar})\"></div>
+                           <p>{$username}</p>
+                           <div class=\"comment\">{$comment}</div>
+                         </div>";
+            
+            // output media
+            echo $content . "</li>";
+          }
+        ?>
+        </ul>
+        <!-- GitHub project -->
+        <footer>
+          <p>created by <a href="https://github.com/cosenary/Instagram-PHP-API">cosenary's Instagram class</a>, available on GitHub</p>
+          <iframe width="95px" scrolling="0" height="20px" frameborder="0" allowtransparency="true" src="http://ghbtns.com/github-btn.html?user=cosenary&repo=Instagram-PHP-API&type=fork&count=true"></iframe>
+        </footer>
+      </div>
     </div>
-    <?
-   }
-?>
-
-<p class="footer"><a href="http://www.webdesignerwall.com/tutorials/css-decorative-gallery/">www.webdesignerwall.com/tutorials/css-decorative-gallery/</a></p>
-
-</body>
+    <!-- javascript -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script>
+      $(document).ready(function() {
+        // rollover effect
+        $('li').hover(
+          function() {
+            var $image = $(this).find('.image');
+            var height = $image.height();
+            $image.stop().animate({ marginTop: -(height - 82) }, 1000);
+          }, function() {
+            var $image = $(this).find('.image');
+            var height = $image.height();
+            $image.stop().animate({ marginTop: '0px' }, 1000);
+          }
+        );
+      });
+    </script>
+  </body>
 </html>
-<?php die(); ?>
--- insta/index.php
+++ insta/index.php
@@ -1,26 +1,53 @@
 <?php
 
-require_once 'Instagram.php';
+require 'instagram.class.php';
 
-$config = array(
-	'client_id' => '359a0cb55e014c2a853b77fed4769564',
-	'client_secret' => 'a03c4453898846abbdadca739b4c1dde',
-	'grant_type' => 'authorization_code',
-	'redirect_uri' => 'http://yashamostofi.com/drinkspls/insta/callback.php'
-);
+// initialize class
+$instagram = new Instagram(array(
+  'apiKey'      => '359a0cb55e014c2a853b77fed4769564',
+  'apiSecret'   => 'a03c4453898846abbdadca739b4c1dde',
+  'apiCallback' => 'http://yashamostofi.com/drinkspls/insta/callback.php'
+));
 
-session_start();
+// create login URL
+$loginUrl = $instagram->getLoginUrl();
 
-if (isset($_SESSION['InstagramAccessToken']) && !empty($_SESSION['InstagramAccessToken']))
-{
-	header('Location: callback.php');
-	die();
-}
-
-$instagram = new Instagram($config);
-$accessToken = $instagram->getAccessToken();
-$_SESSION['InstagramAccessToken'] = $accessToken;
-
-$instagram->setAccessToken($_SESSION['InstagramAccessToken']);
-$instagram->openAuthorizationUrl();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Instagram - OAuth Login</title>
+    <link rel="stylesheet" type="text/css" href="assets/style.css">
+    <style>
+      .login {
+        display: block;
+        font-size: 20px;
+        font-weight: bold;
+        margin-top: 50px;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <header class="clearfix">
+        <h1>Instagram <span>display your photo stream</span></h1>
+      </header>
+      <div class="main">
+        <ul class="grid">
+          <li><img src="assets/instagram-big.png" alt="Instagram logo"></li>
+          <li>
+            <a class="login" href="<? echo $loginUrl ?>">» Login with Instagram</a>
+            <h>Use your Instagram account to login.</h4>
+          </li>
+        </ul>
+        <!-- GitHub project -->
+        <footer>
+          <p>created by <a href="https://github.com/cosenary/Instagram-PHP-API">cosenary's Instagram class</a>, available on GitHub</p>
+        </footer>
+      </div>
+    </div>
+  </body>
+</html>
