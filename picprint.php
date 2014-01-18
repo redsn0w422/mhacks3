66a6451b6cd37c5cd93bb7dee19ac36d3c70e8ec
Yasha Mostofi <yasha.mostofi@gmail.com>
2014-01-18 18:31:43 -0500

    OH MY GOD BECKY

--- picprint.php
+++ picprint.php
@@ -85,7 +85,7 @@ $styleSuffix = rand(1,1000);
 <html>
     <head>
         <title>pic print</title>
-        <link rel='stylesheet' type='text/css' href='picstylesheet.css?id=<?php echo rand(1,1000); ?>'/>
+        <link rel='stylesheet' type='text/css' href='assets/css/picstylesheet.css?id=<?php echo rand(1,1000); ?>'/>
         <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
 		<link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
     </head>
@@ -96,25 +96,41 @@ $styleSuffix = rand(1,1000);
             </div>
 
             <div id="content">
-                <h2>pick your social network</h2>
-                <a href='<?=$fb_loginUrl ?>'>
-                    <img src="facebook-connect-logo.png" alt="Connect with Facebook">
-                </a>
-                </br>
-                <a href='<?=$insta_loginUrl ?>'>
-                    <img src="instagram-connect-logo.png" alt="Connect with Instagram">
-                </a>
-                </br>
-                </br>
+            	
+            	<? if ($user): ?>
+            		<a href='<?=$fb_logoutUrl ?>'>
+            			<img src="assets/img/fb-out.png" alt="Disconnect Facebook">
+            		</a>
+            	<? endif; ?>
+            	<? if (!($user)): ?>
+            		<a href='<?=$fb_loginUrl ?>'>
+            			<img src="assets/img/fb-in.png" alt="Connect with Facebook">
+            		</a>
+            	<? endif; ?>
+            	
+            	</br>
+            	
+            	<? if ($insta_active): ?>
+            		<a href="">
+            			<img src="assets/img/insta-out.png" alt="Disconnect Instagram">
+            		</a>
+            	<? endif; ?>
+            	<? if (!($insta_active)): ?>
+            		<a href='<?=$insta_loginUrl ?>'>
+            			<img src="assets/img/insta-in.png" alt="Connect with Instagram">
+            		</a>
+            	<? endif; ?>          
+
             </div>
+            
 				<form action="quickprint_launch.php" method="post">
-				<? if ($user): ?>
+			<? if ($user): ?>
             	<a href='<?=$logoutUrl ?>'>logout of facebook</a>
             
 			
 				<? foreach ($links as $link): ?>
 					<img src ="https://i.embed.ly/1/display?url=<?= $link ?>&key=f4a9399a56fe4b6eb8ec6cd74c065b0f"/>
-						<input type='checkbox' name='checklist[]' value=<?= $link ?>/>
+						<input type='checkbox' name='checklist[]' value='162.243.204.101/api.php?url='<?= $link ?>'/>
 						</br>
 				<? endforeach ?>
 			<? endif; ?>
@@ -133,7 +149,7 @@ $styleSuffix = rand(1,1000);
 									// output media
 									$image = $media->images->low_resolution->url;
 									$url = $media->images->standard_resolution->url;
-									$content .= "<input type=\"checkbox\" class=\"checkbox\" name=\"checklist[]\" value=\"{$url}\">";
+									$content .= "<input type=\"checkbox\" class=\"checkbox\" name=\"checklist[]\" value=\"162.243.204.101/api.php?url={$url}\">";
 									$content .= "<img class=\"media\" src=\"{$image}\"/>";
 									
 									// create meta section
@@ -153,7 +169,7 @@ $styleSuffix = rand(1,1000);
 				</div>
 			<? endif; ?>
 
-			<? if ($user && $insta_active); ?>
+			<? if ($user && $insta_active): ?>
 				<div class="submit_div">
 					<input class="submit_button" type="submit">
 				</div>
