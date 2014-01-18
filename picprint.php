b41682e5fa63d0b8415fcc6abe3262340ee22595
briantruong777 <briantruong777@gmail.com>
2014-01-18 16:21:26 -0600

    working combined insta and fb

--- picprint.php
+++ picprint.php
@@ -4,9 +4,38 @@ require 'instagram.class.php';
 $instagram = new Instagram(array('apiKey'=>'359a0cb55e014c2a853b77fed4769564',
 'apiSecret'=>'a03c4453898846abbdadca739b4c1dde',
 //'apiCallback'=>'localhost:5/mhacks/instacallback.php'));
-'apiCallback' => 'http://yashamostofi.com/drinkspls/swag/instacallback.php'));
+'apiCallback' => 'http://yashamostofi.com/drinkspls/swag/picprint.php'));
 // create login URL
 $insta_loginUrl = $instagram->getLoginUrl();
+
+// receive OAuth code parameter
+$code = $_GET['code'];
+
+// check whether the user has granted access
+if (isset($code)) {
+
+	try {
+		// receive OAuth token object
+		$data = $instagram->getOAuthToken($code);
+		$username = $username = $data->user->username;
+		
+		// store user access token
+		$instagram->setAccessToken($data);
+
+		// now you have access to all authenticated user methods
+		$result = $instagram->getUserMedia();
+	}
+	catch (Exception $e) {
+//		echo "Caught exception: '{$e->getMessage()}'\n{$e}\n";
+	}
+
+} else {
+
+	// check whether an error occurred
+	if (isset($_GET['error'])) {
+		echo 'An error occurred: ' . $_GET['error_description'];
+	}
+}
 
 // ********** FACEBOOK ******************
 require 'facebook.php';
@@ -58,7 +87,6 @@ $styleSuffix = rand(1,1000);
 		<link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
     </head>
     <body>
-
         <div id="container">
             <div id="header">
                 pic print
@@ -87,6 +115,45 @@ $styleSuffix = rand(1,1000);
 						</br>
 				<? endforeach ?>
 				<input type="submit" value="Choose Selected"/>
+			<? endif; ?>
+
+			<? if (isset($code)): ?>
+				<div class="container">
+						<h1>Instagram photos taken by <?= $data->user->username ?></h1>
+					<div class="main">
+						<form action="quickprint_launch.php" method="post">
+							<ul class="grid">
+								<?php
+								// display all user uploads
+								foreach ($result->data as $media)
+								{
+									$content = "<li>";
+									
+									// output media
+									$image = $media->images->low_resolution->url;
+									$url = $media->images->standard_resolution->url;
+									$content .= "<input type=\"checkbox\" class=\"checkbox\" name=\"checklist[]\" value=\"{$url}\">";
+									$content .= "<img class=\"media\" src=\"{$image}\"/>";
+									
+									// create meta section
+									$avatar = $media->user->profile_picture;
+									$username = $media->user->username;
+									$comment = $media->caption->text;
+									$content .= "<div class=\"content\">
+																 <div class=\"comment\">{$comment}</div>
+															 </div>";
+									
+									// output media
+									echo $content . "</li>";
+								}
+								?>
+							</ul>
+							<div class="submit_div">
+								<input class="submit_button" type="submit">
+							</div>
+						</form>
+					</div>
+				</div>
 			<? endif; ?>
 			
             <div id="footer">
