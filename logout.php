9d679f8164ef2932cc4fa3540f233efac199b2cf
Yasha Mostofi <yasha.mostofi@gmail.com>
2014-01-26 21:51:11 -0600

    Revert d7e074e..ac749bc
    This rolls back to commit d7e074e8b5070d1f28d266798f3f4790f2700593.

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'facebook.php';
-$facebook = new Facebook(array(
-  'appId'  => '1414991652077072',
-  'secret' => 'd7f434a8b143b848819e782d7d184645',
-));
-session_destroy();
-header( "location:picprint.php" );
-?>
