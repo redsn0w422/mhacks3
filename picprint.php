80702e9b9a5d5358a9df378521eed69474c2daf5
kianfotovat <sonofthebrownguy@gmail.com>
2014-06-12 12:30:05 -0500

    maek less uglee text on skreen

--- picprint.php
+++ picprint.php
@@ -120,13 +120,11 @@ if ($user) {
       testAPI();
     } else if (response.status === 'not_authorized') {
       // The person is logged into Facebook, but not your app.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into this app.';
+      document.getElementById('status').innerHTML = '';
     } else {
       // The person is not logged into Facebook, so we're not sure if
       // they are logged into this app or not.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into Facebook.';
+      document.getElementById('status').innerHTML = '';
     }
   }
 
@@ -181,8 +179,7 @@ if ($user) {
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
       console.log('Successful login for: ' + response.name);
-      document.getElementById('status').innerHTML =
-        'Thanks for logging in, ' + response.name + '!';
+      document.getElementById('status').innerHTML = '';
     });
   }
 </script>
